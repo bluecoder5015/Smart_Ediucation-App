d4bd9020cf65bcd1422e7a9a16d0decf21707909
bluecoder_ <yogendrasingh5015@gmail.com>
2020-09-04 18:15:15 +0530

    Added Todo

--- app/src/main/java/com/example/smart_education/TodoFragment.kt
+++ app/src/main/java/com/example/smart_education/TodoFragment.kt
@@ -1,14 +1,20 @@
 package com.example.smart_education
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_todo.*
+import com.example.smart_education.MainActivity
 
 class TodoFragment : Fragment(R.layout.fragment_todo) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        textview.text = "Hello Welcome"
+        todo_add.setOnClickListener {
+            Intent(context,AddTodo::class.java).also{
+                startActivity(it)
+            }
+        }
     }
 }
