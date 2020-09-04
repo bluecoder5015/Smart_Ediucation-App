cb8620d8fc5bf4d6f23af60ba5484e630fd4f1b8
bluecoder_ <yogendrasingh5015@gmail.com>
2020-09-04 18:19:20 +0530

    added todo

---
+++ app/src/main/java/com/example/smart_education/AddTodo.kt
@@ -0,0 +1,12 @@
+package com.example.smart_education
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTodo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addtodo)
+
+    }
+}
---
+++ app/src/main/java/com/example/smart_education/Todos.kt
@@ -0,0 +1,7 @@
+package com.example.smart_education
+
+data class Todos (
+    var title:String,
+    var time:String,
+    var ischecked:Boolean
+)
